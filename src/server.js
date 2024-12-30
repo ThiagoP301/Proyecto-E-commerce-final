f8a3ee9273806d19b6c1c4ecaf08fefbc0b00b31
Thiago Piriz <thiagopiriz19@gmail.com>
2024-12-30 07:51:55 -0300

    trabajo backend

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
-import express, { Router } from 'express';
+import express from 'express';
 import statusRouter from  "./routes/statusRoute.js";
-import authRouter from "./routes/authRoute.js";
-import mongoDB from './config/dbConfig.js'; 
+import authRouter from "./routes/authRoute.js"; 
 import cors from "cors"
 import cartRoute from './routes/cartRoute.js';
 import productRouter from './routes/productsRoute.js';
@@ -20,6 +19,8 @@ app.use("/api/auth", authRouter)
 app.use("/api/cart", cartRoute)
 app.use("/api/products", productRouter)
 
-
+app.listen(PORT, (req, res)=>{
+    console.log(`El servidor se esta ejecutando en http://localhost:${PORT}`)
+})
 
 export default app
